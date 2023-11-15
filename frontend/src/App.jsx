f0e3cdfcc311d75d781297a465c3d8b059f8d722
O. Sharpe <osharpe001@gmail.com>
2023-11-14 23:28:24 -0500

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
-// import { useState } from 'react';
-
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import DetailedDescriptions from "./pages/DetailedDescriptions";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PaymentInfoForm from "./pages/PaymentInfoForm";
+import Register from "./pages/Register";
+import RequestForm from "./pages/RequestForm";
 
 export default function App() {
   return (
     <>
-      <h1>Store Front Example</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/description" element={<DetailedDescriptions />}/>
+        <Route path="/request" element={<RequestForm />}/>
+        <Route path="/pay-info" element={<PaymentInfoForm />}/>
+        <Route path="/sign-in" element={<Login />}/>
+        <Route path="/sign-up" element={<Register />}/>
+      </Routes>
+      <Footer />
     </>
   );
 }
