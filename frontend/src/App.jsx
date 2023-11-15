186b8bb9ebdb64da625d80f9236f2255408f7ee2
O. Sharpe <osharpe001@gmail.com>
2023-11-15 04:03:23 -0500

    started on  Header functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,35 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import DetailedDescriptions from "./pages/DetailedDescriptions";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import PaymentInfoForm from "./pages/PaymentInfoForm";
-import Register from "./pages/Register";
-import RequestForm from "./pages/RequestForm";
+import {
+  DetailedDescriptions,
+  Home,
+  Login,
+  PaymentInfoForm,
+  Register,
+  RequestForm,
+  AboutUs
+} from "./pages";
 
 export default function App() {
-  return (
-    <>
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/description" element={<DetailedDescriptions />}/>
-        <Route path="/request" element={<RequestForm />}/>
-        <Route path="/pay-info" element={<PaymentInfoForm />}/>
-        <Route path="/sign-in" element={<Login />}/>
-        <Route path="/sign-up" element={<Register />}/>
+        <Route path="/" element={<Home
+                                    loggedIn={loggedIn}
+                                    setLoggedIn={setLoggedIn}
+                                  />} />
+        <Route path="/description" element={<DetailedDescriptions />} />
+        <Route path="/request" element={<RequestForm />} />
+        <Route path="/pay-info" element={<PaymentInfoForm />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/sign-in" element={<Login />} />
+        <Route path="/sign-up" element={<Register />} />
       </Routes>
       <Footer />
     </>
-  );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,75 @@
+import mascot from "../assets/images/mascot.png";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 
-export default function Header() {
-  return (
-    <div>Header</div>
-  );
+export default function Header({ loggedIn, setLoggedIn }) {
+
+  const logOut = () => {
+    setLoggedIn(false);
+  };
+
+  const backHome = () => {
+  };
+
+  // NECESSARY TO CHANGE THE "VIEW-POSTER" BUTTON TO "CHANGE-POSTER" WHEN ON POSTER PAGE
+  const currentScreen = useLocation().pathname;
+
+  // SHIFTING-HEADER USESTATES AND FUNCTIONS
+  const [scrollPosition, setScrollPosition] = useState({
+    y: 0
+  });
+  const [prevScrollPosition, setPreviousScrollPositiion] = useState({
+    y: 0
+  })
+  const handleScrollPositionChange = (e) => {
+    setPreviousScrollPositiion({
+      y: scrollPosition.y
+    });
+    setScrollPosition({
+      y: window.scrollY
+    });
+  };
+  window.addEventListener("scroll", handleScrollPositionChange);
+  let headerPos = scrollPosition.y > 0 && scrollPosition.y > prevScrollPosition.y ? "hide" : "show";
+
+  return <header
+    className="nav-header"
+    style={
+      headerPos === "hide" ?
+        {
+          position: "sticky",
+          top: "-100px",
+          animationName: "raise-nav"
+        }
+        :
+        {
+          position: "sticky",
+          top: "0",
+          animationName: "drop-nav"
+        }
+    }
+  >
+    <nav>
+      <div className="banner" onClick={backHome}>
+        <img className="mascot banner-title" src={mascot} alt="mascot" />
+        <p className="banner-title">Web Store Example</p>
+      </div>
+      <div className="nav-items">
+        {
+          currentScreen === "/blog" &&
+          <Link aria-label="On Click" to="" onClick={backHome} className="button">Blogs</Link>
+        }
+        {loggedIn ?
+          <Link onClick={logOut} className="button">Logout</Link>
+          :
+          currentScreen === "/sign-in" ?
+            <Link aria-label="On Click" to="/sign-up" className="button">Register</Link>
+            :
+            <Link aria-label="On Click" to="/sign-in" className="button">LogIn</Link>
+        }
+      </div>
+    </nav>
+  </header>
 }
