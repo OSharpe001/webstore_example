ebdc389d341f634f6e91aa7b7d58abf3141263e4
O. Sharpe <osharpe001@gmail.com>
2023-11-15 03:58:45 -0500

    created simple director

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as AboutUs } from "./AboutUs";
+export { default as DetailedDescriptions } from "./DetailedDescriptions";
+export { default as Home } from "./Home";
+export { default as Login } from "./Login";
+export { default as PaymentInfoForm } from "./PaymentInfoForm";
+export { default as Register } from "./Register";
+export { default as RequestForm } from "./RequestForm";
