b42c39786991986e3e3362428c47b5db9bca1de3
O. Sharpe <osharpe001@gmail.com>
2023-11-21 21:43:16 -0500

    merch photo set up file

---
+++ frontend/src/components/merchItems.js
@@ -0,0 +1,54 @@
+import assorted1 from "../assets/images/items/assorted1.jpeg";
+import assorted2 from "../assets/images/items/assorted2.jpg";
+import boy1 from "../assets/images/items/boy1.jpg";
+import electric_mouse from "../assets/images/items/electric_mouse.jpg";
+import girl1 from "../assets/images/items/girl1.jpg";
+import hats1 from "../assets/images/items/hats1.jpg";
+import mermaids1 from "../assets/images/items/mermaids1.jpg";
+import plumber from "../assets/images/items/plumber.jpg";
+
+const merchItems = [
+    {
+        picNumber: 0,
+        photo: plumber,
+        alt: "knitted plumber"
+    },
+    {
+        picNumber: 1,
+        photo: assorted1,
+        alt: "assorted knits"
+    },
+    {
+        picNumber: 2,
+        photo: assorted2,
+        alt: "assorted knits"
+    },
+    {
+        picNumber: 3,
+        photo: boy1,
+        alt: "knitted boy"
+    },
+    {
+        picNumber: 4,
+        photo: electric_mouse,
+        alt: "knitted electric mouse"
+    },
+    {
+        picNumber: 5,
+        photo: girl1,
+        alt: "knitted girl"
+    },
+    {
+        picNumber: 6,
+        photo: hats1,
+        alt: "knitted hats"
+    },
+    {
+        picNumber: 7,
+        photo: mermaids1,
+        alt: "knitted mermaid"
+    },
+];
+
+
+export default merchItems;
