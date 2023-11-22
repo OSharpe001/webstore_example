97858b2c54bc210f9a8b78565c8ac3defeb10bae
O. Sharpe <osharpe001@gmail.com>
2023-11-21 21:42:27 -0500

    starting home page layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+
 
 
 export default function Home() {
-  return <main>
-    Home
+  return <main className="home-page">
+    <Hero />
+    <h1 className="direct"><span className="underlined">Or</span> make a <Link aria-label="On Click" to="/request">Request</Link></h1>
   </main>
 }
