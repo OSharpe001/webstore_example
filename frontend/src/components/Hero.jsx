7729ec57c0399dd2bbe33e6dd71f0e7689dd34dd
O. Sharpe <osharpe001@gmail.com>
2023-11-23 19:17:58 -0500

    took merch photos out of hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,27 +1,28 @@
-import { useState } from "react";
-import merchItems from "./merchItems";
-import white_left_arrow from "../assets/images/icons/white_left_arrow.png"
-import white_right_arrow from "../assets/images/icons/white_right_arrow.png"
+// import { useState } from "react";
+// import white_left_arrow from "../assets/images/icons/white_left_arrow.png";
+// import white_right_arrow from "../assets/images/icons/white_right_arrow.png";
+import owner from "../assets/images/owner.jpg";
 
 
 export default function Hero() {
 
-  const [currentItemPic, setCurrentItemPic] = useState(merchItems[0]);
+  // const [currentItemPic, setCurrentItemPic] = useState(merchItems[0]);
 
-  const changeHeroImage = (direction) => {
-    if (direction === "up" && currentItemPic.picNumber < merchItems.length - 1) {
-      setCurrentItemPic(merchItems[currentItemPic.picNumber + 1]);
-    } else if (direction === "down" && currentItemPic.picNumber > 0) {
-      setCurrentItemPic(merchItems[currentItemPic.picNumber - 1]);
-    }
-  };
+  // const changeHeroImage = (direction) => {
+  //   if (direction === "up" && currentItemPic.picNumber < merchItems.length - 1) {
+  //     setCurrentItemPic(merchItems[currentItemPic.picNumber + 1]);
+  //   } else if (direction === "down" && currentItemPic.picNumber > 0) {
+  //     setCurrentItemPic(merchItems[currentItemPic.picNumber - 1]);
+  //   }
+  // };
 
   return <section className="hero-section">
     <p className="speech">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur optio illo amet, hic corrupti maiores nihil porro dolore quaerat, pariatur eos sit. Non porro cupiditate nobis, similique possimus ratione totam.</p>
-    <div className="picture-box">
+    <img src={owner} alt="CEO" className="ceo-pic" />
+    {/* <div className="picture-box">
       <button onClick={()=>changeHeroImage("down")} className="change-pic-button left"><img className="change-pic-arrow" src={white_left_arrow} alt={"white_left_arrow"} /></button>
       <button><img className="merch-item" src={currentItemPic.photo} alt={currentItemPic.alt} /></button>
       <button onClick={()=>changeHeroImage("up")} className="change-pic-button right"><img className="change-pic-arrow" src={white_right_arrow} alt={"white_right_arrow"} /></button>
-    </div>
+    </div> */}
   </section>
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,33 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import merchItems from "./merchItems";
+import white_left_arrow from "../assets/images/icons/white_left_arrow.png";
+import white_right_arrow from "../assets/images/icons/white_right_arrow.png";
 import Hero from "../components/Hero";
 
 
 
 export default function Home() {
+
+  const navigate = useNavigate();
+  const [currentItemPic, setCurrentItemPic] = useState(merchItems[0]);
+
+  const changeHeroImage = (direction) => {
+    if (direction === "up" && currentItemPic.picNumber < merchItems.length - 1) {
+      setCurrentItemPic(merchItems[currentItemPic.picNumber + 1]);
+    } else if (direction === "down" && currentItemPic.picNumber > 0) {
+      setCurrentItemPic(merchItems[currentItemPic.picNumber - 1]);
+    }
+  };
+
   return <main className="home-page">
     <Hero />
+    <div className="picture-box">
+      <button onClick={()=>changeHeroImage("down")} className="change-pic-button left"><img className="change-pic-arrow" src={white_left_arrow} alt={"white_left_arrow"} /></button>
+      <button><img className="merch-item" src={currentItemPic.photo} alt={currentItemPic.alt} /></button>
+      <button onClick={()=>changeHeroImage("up")} className="change-pic-button right"><img className="change-pic-arrow" src={white_right_arrow} alt={"white_right_arrow"} /></button>
+    </div>
     <h1 className="direct"><span className="underlined">Or</span> make a <Link aria-label="On Click" to="/request">Request</Link></h1>
+    <button className="button" onClick={()=>navigate("/request")}>Special Request</button>
   </main>
 }
